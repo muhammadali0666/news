08ecd554e0f76da1e26a4ba17c0471a0fff64aff
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2022-06-07 15:59:51 +0500

    init

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const elMovieList = document.querySelector(".movie__list");
+const elResult = document.querySelector(".movie__result-num");
+
+elResult.textContent = daryoPostlari.length;
+
+for (let news of daryoPostlari) {
+
+  const newLi = document.createElement("li");
+  const newImg = document.createElement("img");
+  const newDiv = document.createElement("div");
+  const newTitle = document.createElement("h5");
+  const newLanguage = document.createElement("p");
+  const newYear = document.createElement("p");
+  const newButton = document.createElement("a");
+
+  newLi.setAttribute("class", "card mb-3");
+  newLi.style.width = "18rem";
+  newImg.classList.add("card-img-top");
+  newImg.setAttribute("src", news.photo);
+  newDiv.classList.add("card-body");
+  newTitle.classList.add("card-title");
+  newLanguage.classList.add("card-text");
+  newYear.classList.add("card-text");
+  newButton.setAttribute("class", "btn btn-danger");
+  newButton.setAttribute("href", `https://daryo.uz/${news.daryoId}`);
+
+  newTitle.textContent = news.title;
+  newLanguage.textContent = news.date;
+ 
+  newButton.textContent ="link";
+  newImg.textContent = "photo"
+
+  elMovieList.appendChild(newLi);
+  newLi.appendChild(newImg);
+  newLi.appendChild(newDiv);
+  newDiv.appendChild(newTitle);
+  newDiv.appendChild(newLanguage);
+  newDiv.appendChild(newYear);
+  newDiv.appendChild(newButton);
+}
